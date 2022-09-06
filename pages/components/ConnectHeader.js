63b9b92d317134a04879264083971d6f4abded79
Alex Biet <alexandrubiet@gmail.com>
2022-09-07 01:10:23 +0200

    Fixed merge confl

--- pages/components/ConnectHeader.js
+++ pages/components/ConnectHeader.js
@@ -45,24 +45,6 @@ export default function ConnectHeader(props) {
             {active ? (
                 <>
                  
-                    <div className='mt-5 mr-5 align-middle justify-self-end'>
-                        <WorldIDWidget
-                            actionId="wid_staging_ed5840f20f5c5ac1b07a144005e45d6a" // obtain this from developer.worldcoin.org
-                            signal="my_signal"
-                            enableTelemetry
-                            onSuccess={(verificationResponse) => console.log(verificationResponse)} // you'll actually want to pass the proof to the API or your smart contract
-                            onError={(error) => console.error(error)}
-                        />
-                        <div className='flex flex-col justify-center'>
-                            <p className="text-l font-bold">Connected account:</p>
-                            <p className="text-l">{Number(balance).toFixed(2)} {chainIdsMap[chainId]}</p>
-                            <p className="text-l">{account.substring(0, 6) + "..." + account.slice(-4)}</p>
-                            <div className='flex gap-2 justify-end'>
-                            <button className="text-purple-500" onClick={() => switchNetwork(mumbai)}>Mumbai</button>
-                            <button className="text-red-500" onClick={() => switchNetwork(goerli)}>Goerli</button>
-                            </div>
-                        </div>
-                    </div>
                   <div className='my-5 mr-5 align-middle justify-self-center'>
                     <WorldIDWidget
                         actionId="wid_staging_ed5840f20f5c5ac1b07a144005e45d6a" // obtain this from developer.worldcoin.org
@@ -74,7 +56,7 @@ export default function ConnectHeader(props) {
                   </div>
 
                   <div className='my-5 mr-5 align-middle justify-self-end'>
-                      <p className="text-l"><b>Connected account:</b> {account.substring(0, 6) + "..." + account.slice(-4)} ({balance} {symbol})</p>
+                      <p className="text-l"><b>Connected account:</b> {account.substring(0, 6) + "..." + account.slice(-4)} ({Number(balance).toFixed(2)} {chainIdsMap[chainId]})</p>
                       <div className='flex gap-2 justify-end'>
                       <button className="text-purple-500" onClick={() => switchNetwork(mumbai)}>Mumbai</button>
                       <button className="text-red-500" onClick={() => switchNetwork(goerli)}>Goerli</button>
