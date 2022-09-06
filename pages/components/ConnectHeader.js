33de793c43d033e5305865b61ac17dcce2d83ee0
Alex Biet <alexandrubiet@gmail.com>
2022-09-07 00:06:20 +0200

    Title, nav optimisation & short wallet address

--- pages/components/ConnectHeader.js
+++ pages/components/ConnectHeader.js
@@ -36,12 +36,14 @@ export default function ConnectHeader(props) {
       };
 
     return (
-        <header className="sticky top-3 columns-2 px-5 pt-5 bg-neutral-100 rounded-xl shadow-xl backdrop-blur-md bg-white/30 nav-border" style={{borderRadius: 16 + 'px'}}>
-          <img src="/logo.svg" className="h-24" /> 
+        <header className="sticky top-3 columns-2 px-5 pt-5 bg-neutral-100 rounded-xl shadow-xl backdrop-blur-md bg-white/30 nav-border grid justify-items-stretch grid-cols-2 gap-4" style={{borderRadius: 16 + 'px'}}>
+          <div className='justify-self-start'>
+            <img src="/logo.svg" className="h-24 " /> 
+          </div>
             {active ? (
                 <>
                  
-                    <div className='flex mt-5 mr-10 justify-end'>
+                    <div className='mt-5 mr-5 align-middle justify-self-end'>
                         <WorldIDWidget
                             actionId="wid_staging_ed5840f20f5c5ac1b07a144005e45d6a" // obtain this from developer.worldcoin.org
                             signal="my_signal"
@@ -51,7 +53,7 @@ export default function ConnectHeader(props) {
                         />
                         <div className='flex flex-col justify-center'>
                             <p className="text-l font-bold">Connected account:</p>
-                            <p className="text-l">{account}</p>
+                            <p className="text-l">{account.substring(0, 6) + "..." + account.slice(-4)}</p>
                             <div className='flex gap-2 justify-end'>
                             <button className="text-purple-500" onClick={() => switchNetwork(mumbai)}>Mumbai</button>
                             <button className="text-red-500" onClick={() => switchNetwork(goerli)}>Goerli</button>
@@ -60,8 +62,8 @@ export default function ConnectHeader(props) {
                     </div>
                 </>
                 ) : (
-                <div className='flex mt-5 mr-10 justify-end'>
-                    <button className="px-4 py-1 text-sm text-blue-600 font-semibold rounded-full border border-blue-200 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2"onClick={() => handleClickConnect()}>Connect</button>
+                <div className="mt-5 mr-5 align-middle justify-self-end">
+                    <button className="align-middle px-4 py-1 text-md text-blue-600 font-semibold rounded-full border border-blue-200 bg-gradient-to-r from-indigo-100 from-indigo-500 hover:bg-gradient-to-l hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2" onClick={() => handleClickConnect()}>Connect</button>
                 </div>
             )}
         </header>
