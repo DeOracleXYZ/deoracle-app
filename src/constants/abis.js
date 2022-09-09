d5753409af2f9b2dc09ea6989d0019c9cd920b4e
Markeljan <Markeljan19@gmail.com>
2022-09-09 18:39:03 -0400

    requestList

--- src/constants/abis.js
+++ src/constants/abis.js
@@ -32,6 +32,66 @@ module.exports = {
       {
         "inputs": [
           {
+            "components": [
+              {
+                "internalType": "string",
+                "name": "requestText",
+                "type": "string"
+              },
+              {
+                "internalType": "address",
+                "name": "requestOrigin",
+                "type": "address"
+              },
+              {
+                "internalType": "uint256",
+                "name": "bounty",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256",
+                "name": "reputation",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256",
+                "name": "maxAnswers",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256[]",
+                "name": "submittedAnswers",
+                "type": "uint256[]"
+              },
+              {
+                "internalType": "bool",
+                "name": "active",
+                "type": "bool"
+              },
+              {
+                "internalType": "uint256",
+                "name": "timeStampPosted",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256",
+                "name": "timeStampDue",
+                "type": "uint256"
+              }
+            ],
+            "internalType": "struct deOracle.Request",
+            "name": "_newRequest",
+            "type": "tuple"
+          }
+        ],
+        "name": "submitRequest",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
             "internalType": "address",
             "name": "signal",
             "type": "address"
@@ -78,12 +138,126 @@ module.exports = {
       },
       {
         "inputs": [],
+        "name": "getRequestList",
+        "outputs": [
+          {
+            "components": [
+              {
+                "internalType": "string",
+                "name": "requestText",
+                "type": "string"
+              },
+              {
+                "internalType": "address",
+                "name": "requestOrigin",
+                "type": "address"
+              },
+              {
+                "internalType": "uint256",
+                "name": "bounty",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256",
+                "name": "reputation",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256",
+                "name": "maxAnswers",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256[]",
+                "name": "submittedAnswers",
+                "type": "uint256[]"
+              },
+              {
+                "internalType": "bool",
+                "name": "active",
+                "type": "bool"
+              },
+              {
+                "internalType": "uint256",
+                "name": "timeStampPosted",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256",
+                "name": "timeStampDue",
+                "type": "uint256"
+              }
+            ],
+            "internalType": "struct deOracle.Request[]",
+            "name": "",
+            "type": "tuple[]"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
         "name": "owner",
         "outputs": [
           {
             "internalType": "address",
             "name": "",
             "type": "address"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "name": "requestList",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "requestText",
+            "type": "string"
+          },
+          {
+            "internalType": "address",
+            "name": "requestOrigin",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "bounty",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "reputation",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "maxAnswers",
+            "type": "uint256"
+          },
+          {
+            "internalType": "bool",
+            "name": "active",
+            "type": "bool"
+          },
+          {
+            "internalType": "uint256",
+            "name": "timeStampPosted",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "timeStampDue",
+            "type": "uint256"
           }
         ],
         "stateMutability": "view",
