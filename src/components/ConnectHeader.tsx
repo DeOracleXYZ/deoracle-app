beb95c6da706c718d7b18a47505c602498e64ed1
Alex Biet <alexandrubiet@gmail.com>
2022-09-21 01:40:27 +0200

    Update ConnectHeader.tsx

--- src/components/ConnectHeader.tsx
+++ src/components/ConnectHeader.tsx
@@ -132,6 +132,31 @@ export default function ConnectHeader(props: any) {
                         d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
                       />
                     </svg>{" "}
+                    <Image
+                      className={worldIdCondition}
+                      onClick={() =>
+                        console.log(
+                          worldIdVerified
+                            ? "World ID verified."
+                            : "Not World ID verified."
+                        )
+                      }
+                      src="/world-id.svg"
+                      height="24"
+                      width="24"
+                      alt="World ID verified"
+                    />
+
+                    <Image
+                      className={ENSCondition}
+                      onClick={() =>
+                        console.log(ENSVerified ? "ENS verified." : "Not ENS verified.")
+                      }
+                      src="/ens.svg"
+                      height="24"
+                      width="24"
+                      alt="ENS verified"
+                    />
                     {verificationCount
                       ? "Verified " + verificationCount + "/2"
                       : "Unverified 0/2"}
@@ -180,33 +205,8 @@ export default function ConnectHeader(props: any) {
             <b>Verification ({verificationCount}/2):</b>
           </p>
           <div className="flex gap-3 pb-3">
-            <Image
-              className={`${worldIdCondition}` + " hover:opacity-50"}
-              onClick={() =>
-                console.log(
-                  worldIdVerified
-                    ? "World ID verified."
-                    : "Not World ID verified."
-                )
-              }
-              src="/world-id.svg"
-              height="30"
-              width="30"
-              alt="World ID verified"
-            />
-
-            <Image
-              className={`${ENSCondition}` + " hover:opacity-50"}
-              onClick={() =>
-                console.log(ENSVerified ? "ENS verified." : "Not ENS verified.")
-              }
-              src="/ens.svg"
-              height="30"
-              width="30"
-              alt="ENS verified"
-            />
             {!worldIdVerified && WorldIDWidget}
-            {(ENSName && !ENSVerified) && <button onClick={verifyENS}>Verify ENS</button>}
+            {(ENSName && !ENSVerified) && <button onClick={verifyENS} className="">Verify ENS</button>}
           </div>
           <hr />
           <p className="pt-3 text-slate-500">
